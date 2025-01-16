ab76bef7c899b46c1ec7b3b810bef1c75a962e01
dev-mraj <meghraj@middleware.io>
2025-01-16 21:25:30 +0530

    init

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pkg script.js --output yaml-transformer --targets node16-linux-x64,node16-win-x64,node16-macos-x64
+
+pkg script.js  --out-path ./bin/ --targets node16-linux-x64,node16-linux-arm64,node16-win-x64
